49a82f69d2261f6fa9de70d1347b533f65fe9683
josephdiaa <joseph.diaa@gmail.com>
2023-02-17 15:22:27 -0800

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 16; i++)
+	{
+		if (i < 10)
+		{
+			putchar('0' + i);
+		}
+		else
+		{
+			putchar('a' + (i - 10));
+		}
+	}
+	putchar('\n');
+	return (0);
+}
