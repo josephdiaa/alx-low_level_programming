4f8deac77c5f74587837029cc0c45592a00ab3e3
josephdiaa <joseph.diaa@gmail.com>
2023-02-17 12:52:59 -0800

    1. The last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("%d", n % 10);
+	return (0);
+}
