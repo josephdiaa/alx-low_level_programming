20dc64810e38d2bf78472aa71c41a379b3b648be
josephdiaa <joseph.diaa@gmail.com>
2023-03-03 18:01:22 -0800

    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

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,17 +9,21 @@
  */
 void print_number(int n)
 {
+	unsigned int n1;
+
+	n1 = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = n * -1;
+		n1 = -n;
 	}
-	if (n / 10 != 0)
-	{
-		print_number(n / 10);
-	}
-	char s1 = (n % 10) + '0';
 
-	_putchar(s1);
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
 }
+
 
